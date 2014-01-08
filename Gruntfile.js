e4e2bcbfa7e74e262e686ea4689361789e8e69bf
charlespeters <charlespeters42@gmail.com>
2014-01-08 13:04:29 -0500

    ...working...

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,6 @@ module.exports = function(grunt) {
         target:{
             options: {
                 port: 9001,
-                keepalive: true
             }
         }
     },
@@ -54,6 +53,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-notify');
 
 
   // Default task(s).
