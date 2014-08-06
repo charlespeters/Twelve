70a79649e53a49dc3f56805a1292b29b2defd8ca
charlespeters <charlespeters42@gmail.com>
2014-08-05 23:21:06 -0400

    Cleaning up files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,5 @@
 var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
+    sass         = require('gulp-ruby-sass'),
     plumber      = require('gulp-plumber'),
     autoprefixer = require('gulp-autoprefixer'),
     connect      = require('gulp-connect');
@@ -7,7 +7,7 @@ var gulp         = require('gulp'),
 gulp.task('styles', function(){
     gulp.src('src/twelve/**/*.scss')
         .pipe(plumber())
-        .pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}))
+        .pipe(sass({sourcemap: true, style: 'compressed'}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('dist/css/'))
         .pipe(connect.reload());
