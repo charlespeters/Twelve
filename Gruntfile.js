81b9115ee4632b17445a0a3c5c24874d3bbea392
charlespeters <charlespeters42@gmail.com>
2014-08-23 00:58:58 -0400

    switching from gulp to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+    // Project configuration.
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: true
+                },
+                files: {
+                    'build/css/twelve.css': 'src/scss/twelve.scss',
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 version', 'ie 8', 'ie 9']
+            },
+            single_file: {
+                src: 'dist/css/twelve.css',
+                dest: 'dist/css/twelve.prefixed.css'
+            },
+        },
+        cssmin: {
+            minify: {
+                src: 'dist/css/twelve.prefixed.css',
+                dest: 'dist/css/twelve.min.css',
+            }
+        },
+        connect: {
+            server: {
+              options: {
+                port: 9001,
+                base: 'dist/',
+                open: true,
+                livereload: true
+              }
+            }
+        },
+        bake: {
+            your_target: {
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
+            },
+        },
+        watch: {
+            html: {
+                files: ['src/*.html', 'src/inc/*.html'],
+                tasks: ['bake'],
+                options: { livereload: 35729 }
+            },
+            css: {
+                files: ['src/scss/*.scss','src/scss/**/*.scss'],
+                tasks: ['sass', 'autoprefixer', 'cssmin'],
+                options: { livereload: 35729 }
+            },
+        },
+    });
+
+    // Loaded tasks
+    grunt.loadNpmTasks( 'grunt-contrib-sass' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+    grunt.loadNpmTasks( 'grunt-contrib-connect' );
+    grunt.loadNpmTasks( 'grunt-autoprefixer' );
+    grunt.loadNpmTasks( 'grunt-notify' );
+    grunt.loadNpmTasks( 'grunt-bake' );
+
+    // Registered task(s).
+    grunt.registerTask('default', ['notify']);
+    grunt.registerTask('build', ['sass', 'autoprefixer', 'cssmin', 'bake']);
+    grunt.registerTask('dev', ['connect', 'watch']);
+};
