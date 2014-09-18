a1f306e8bd5560c9325645fe985f6b6e4c2f9935
charlespeters <charlespeters42@gmail.com>
2014-09-17 22:00:40 -0400

    getting started.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+    // Project configuration.
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                },
+                files: {
+                    'dist/css/twelve.css': 'src/twelve/twelve.scss',
+                    'dist/css/app.css': 'src/app.scss'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 version', 'ie 8', 'ie 9']
+            },
+            single_file: {
+                src: 'dist/css/twelve.css',
+                dest: 'dist/css/twelve.prefixed.css'
+            },
+        },
+        cssmin: {
+            minify: {
+                src: 'dist/css/twelve.prefixed.css',
+                dest: 'dist/css/twelve.min.css',
+            }
+        },
+        connect: {
+            server: {
+              options: {
+                port: 9001,
+                base: './',
+                open: true,
+                livereload: true
+              }
+            }
+        },
+        watch: {
+            css: {
+                files: ['stylesheets/*.scss','stylesheets/twelve/*.scss','stylesheets/twelve/**/*.scss'],
+                tasks: ['sass', 'autoprefixer', 'cssmin'],
+                options: { livereload: 35729 }
+            },
+        },
+    });
+
+    // Loaded tasks
+    grunt.loadNpmTasks( 'grunt-contrib-sass' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+    grunt.loadNpmTasks( 'grunt-contrib-connect' );
+    grunt.loadNpmTasks( 'grunt-autoprefixer' );
+    grunt.loadNpmTasks( 'grunt-notify' );
+    grunt.loadNpmTasks( 'grunt-bake' );
+
+    // Registered task(s).
+    grunt.registerTask('default', ['notify']);
+    grunt.registerTask('build', ['sass', 'autoprefixer', 'cssmin', 'bake']);
+    grunt.registerTask('dev', ['connect', 'watch']);
+};
