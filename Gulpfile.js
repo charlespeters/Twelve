f47c410377bc30882325515970e62b8f92d456a0
charles peters <charlespeters42@gmail.com>
2015-04-05 02:40:38 -0400

    Updating project structure and working on the gird

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,23 +3,39 @@ var gulp         = require('gulp'),
     plumber      = require('gulp-plumber'),
     autoprefixer = require('gulp-autoprefixer'),
     size         = require('gulp-size'),
-    pixrem       = require('gulp-pixrem');
+    pixrem       = require('gulp-pixrem'),
+    browserSync  = require('browser-sync'),
+    reload       = browserSync.reload;
 
 gulp.task('styles', function(){
   gulp.src(['app.scss', '_twelve.scss','./src/**/*.scss'])
   .pipe(plumber())
-  .pipe(sass({
-    outputStyle: 'compressed'
-  }))
+  .pipe(sass({outputStyle: 'compressed'}))
   .pipe(autoprefixer('last 2 version'))
   .pipe(pixrem())
   .pipe(size())
-  .pipe(gulp.dest('css/'));
+  .pipe(gulp.dest('./app/'))
+  .pipe(reload({stream: true}));
+});
+
+gulp.task('html', function(){
+  gulp.src('./app/index.html')
+  .pipe(plumber())
+  .pipe(reload({stream: true}));
+});
+
+gulp.task('connect', function() {
+  browserSync({
+    server: {
+      baseDir: './app'
+    }
+  });
 });
 
 
 gulp.task('watch', function(){
-  gulp.watch(['_twelve.scss','./src/**/*.scss'], ['styles']);
+  gulp.watch(['app.scss', '_twelve.scss','./src/**/*.scss'], ['styles']);
+  gulp.watch('./app/index.html', ['html']);
 });
 
-gulp.task('default', ['styles', 'watch']);
+gulp.task('default', ['styles', 'connect', 'watch']);
